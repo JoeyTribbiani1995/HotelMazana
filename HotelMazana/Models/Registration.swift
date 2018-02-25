d60ca26364d49abcd3936d703b4d64eea2f0fdd4
JoeyTribbiani <vophanhongdung@gmail.com>
2018-02-25 08:37:19 +0700

    first commit

---
+++ HotelMazana/Models/Registration.swift
@@ -0,0 +1,9 @@
+//
+//  Registration.swift
+//  HotelMazana
+//
+//  Created by Dũng Võ on 2/23/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# HotelMazana
