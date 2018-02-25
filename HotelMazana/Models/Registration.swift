be0ca52ccb2f5a1866ebf1df79796d1b24ac5e07
JoeyTribbiani <vophanhongdung@gmail.com>
2018-02-25 08:40:01 +0700

    sencond commit

--- HotelMazana/Models/Registration.swift
+++ HotelMazana/Models/Registration.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+struct Registration {
+    var firstName : String
+    var lastName : String
+    var emailAddress : String
+    
+    var checkInDate : Date
+    var checkOutData : Date
+    var numberOfAdults : Int
+    var numberOfChildren : Int
+    
+    var roomType : RoomType
+    var wifi : Bool
+    
+    init(firstName : String , lastName : String , email : String , checkInDate : Date , checkOutDate : Date , numberOfAdults : Int , numberOfChildren : Int , roomType : RoomType, wifi : Bool) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.emailAddress = email
+        self.checkInDate = checkInDate
+        self.checkOutData = checkOutDate
+        self.numberOfAdults = numberOfAdults
+        self.numberOfChildren = numberOfChildren
+        self.roomType = roomType
+        self.wifi = wifi
+    }
+}
+
+struct RoomType : Equatable {
+    var id : Int
+    var name : String
+    var shortName : String
+    var price : Int
+    
+    static var all : [RoomType] {
+        return [RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+                RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+                RoomType(id: 2, name: "Penthouse Suite", shortName: "PHS", price: 309)
+        ]
+    }
+}
+
+func ==(lhs : RoomType , rhs : RoomType ) -> Bool {
+    return lhs.id == rhs.id
+}
+
+
--- HotelMazana/View Controllers/AddRegistrationTableViewController.swift
+++ HotelMazana/View Controllers/AddRegistrationTableViewController.swift
@@ -8,88 +8,251 @@
 
 import UIKit
 
-class AddRegistrationTableViewController: UITableViewController {
+class AddRegistrationTableViewController: UITableViewController , SelectRoomTypeTableViewControllerDelegate {
+    
+    func didSelect(roomType: RoomType) {
+        self.roomType = roomType
+        updateRoomType()
+    }
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBOutlet weak var checkInDateLabel: UILabel!
+    @IBOutlet weak var checkOutDateLabel: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var adultsLabel: UILabel!
+    @IBOutlet weak var childrenLabel: UILabel!
+    @IBOutlet weak var numberAdultsStepper: UIStepper!
+    @IBOutlet weak var numberChildrenStepper: UIStepper!
+    
+    @IBOutlet weak var costWifiLabel: UILabel!
+    @IBOutlet weak var wifiSwitch: UISwitch!
+    
+    @IBOutlet weak var roomTypeLabel: UILabel!
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var numberOfNights: UILabel!
+    @IBOutlet weak var RomType: UILabel!
+    @IBOutlet weak var wifi: UILabel!
+    @IBOutlet weak var total: UILabel!
+    
+    //variable status date time picker
+    let checkInDateTimePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDateTimePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    
+    var isCheckInDateTimePickerShown : Bool = false {
+        didSet{
+            checkInDatePicker.isHidden = !isCheckInDateTimePickerShown
+        }
+    }
+    var isCheckOutDateTimePickerShown : Bool = false {
+        didSet{
+            checkOutDatePicker.isHidden = !isCheckOutDateTimePickerShown
+        }
+    }
+    
+    var roomType : RoomType?
+    
+    var registration : Registration?
+    {
+        guard let roomType = roomType else {
+            return nil
+        }
+
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
+
+        let numberAdults = Int(numberAdultsStepper.value)
+        let numberChildren = Int(numberChildrenStepper.value)
+
+        let wifiCost = wifiSwitch.isOn
+
+        return Registration(firstName: firstName, lastName: lastName, email: email, checkInDate: checkInDate, checkOutDate: checkOutDate, numberOfAdults: numberAdults, numberOfChildren: numberChildren, roomType: roomType, wifi: wifiCost)
+    }
+    
+    var registrationPrepare : Registration?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
+        
+        //update date picker view
+        updateDateViews()
+        
+        //update stepper value
+        updateNumberOfGuests()
+        
+        //cost wifi
+        costWifiLabel.text = "$10 \t"
+        
+        //update room type
+        updateRoomType()
+        
+        //update registration detail
+        if let registrationPrepare = registrationPrepare {
+            firstNameTextField.text = registrationPrepare.firstName
+            lastNameTextField.text = registrationPrepare.lastName
+            emailTextField.text = registrationPrepare.emailAddress
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            checkInDateLabel.text = dateFormatter.string(from: registrationPrepare.checkInDate)
+            checkOutDateLabel.text = dateFormatter.string(from: registrationPrepare.checkOutData)
+            adultsLabel.text = String(registrationPrepare.numberOfAdults)
+            childrenLabel.text = String(registrationPrepare.numberOfChildren)
+            roomTypeLabel.text = registrationPrepare.roomType.name
+        }
+        
+        updateDoneButton()
+        
+    
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    func updateDateViews(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInDateLabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @IBAction func datePickerValueChanged(_ sender : UIDatePicker){
+        updateDateViews()
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    //cell heights
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch (indexPath.section , indexPath.row) {
+        case (checkInDateTimePickerCellIndexPath.section , checkInDateTimePickerCellIndexPath.row):
+            if isCheckInDateTimePickerShown {
+                return 216.0
+            }else {
+                return 0.0
+            }
+        case (checkOutDateTimePickerCellIndexPath.section , checkOutDateTimePickerCellIndexPath.row):
+            if isCheckOutDateTimePickerShown {
+                return 216.0
+            }else {
+                return 0.0
+            }
+        default:
+            return 44.0
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch (indexPath.section , indexPath.row) {
+        case (checkInDateTimePickerCellIndexPath.section , checkInDateTimePickerCellIndexPath.row - 1):
+            if isCheckInDateTimePickerShown {
+                isCheckInDateTimePickerShown = false
+            }else if isCheckInDateTimePickerShown{
+                isCheckOutDateTimePickerShown = false
+                isCheckInDateTimePickerShown = true
+            }else {
+                isCheckInDateTimePickerShown = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        case (checkOutDateTimePickerCellIndexPath.section , checkOutDateTimePickerCellIndexPath.row - 1) :
+            if isCheckOutDateTimePickerShown {
+                isCheckOutDateTimePickerShown = false
+            }else if isCheckOutDateTimePickerShown{
+                isCheckInDateTimePickerShown = false
+                isCheckOutDateTimePickerShown = true
+            }else {
+                isCheckOutDateTimePickerShown = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        default:
+            break
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func updateNumberOfGuests() {
+        adultsLabel.text = "\(Int(numberAdultsStepper.value)) \t"
+        childrenLabel.text = "\(Int(numberChildrenStepper.value)) \t"
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        updateNumberOfGuests()
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    @IBAction func wifiSwitchChanged(_ sender: UISwitch) {
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    func updateRoomType() {
+        if let roomType = roomType {
+            roomTypeLabel.text = roomType.name
+        }else {
+            roomTypeLabel.text = "No set"
+        }
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "DetailRoomType"{
+            let destinationViewController = segue.destination as? SelectRoomTypeTableViewController
+            destinationViewController?.delegate = self
+            destinationViewController?.roomType = roomType
+        }
     }
-    */
 
+    func updateDoneButton(){
+        
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        
+        doneBarButton.isEnabled = !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty 
+    }
+    
+    @IBAction func textEditingChanged(_ sender : UITextField){
+        updateDoneButton()
+    }
+    
+//    func totalPrice(registration : Registration){
+//        let numberOfNights = Int(registration.checkInDate.timeIntervalSince1970 - registration.checkOutData.timeIntervalSince1970)
+//
+//        let roomTypePrice : Int = registration.roomType.price
+//        let shortNameRoom : String = registration.roomType.shortName
+//
+//        var wifiDaysPrice : Int = 0
+//
+//        if registration.wifi {
+//            wifiDaysPrice = 10 * numberOfNights
+//        }
+//
+//        let total : Double = Double(wifiDaysPrice + roomTypePrice)
+//
+//        updatePrice(roomPrice: roomTypePrice, wifiDaysPrice: wifiDaysPrice, totalPrice: total, numberNights: numberOfNights, shortNameRoom: shortNameRoom)
+//        print(numberOfNights)
+//    }
+//
+//    func updatePrice(roomPrice : Int , wifiDaysPrice : Int , totalPrice : Double  ,numberNights : Int , shortNameRoom : String) {
+////        numberOfNights.text = "\(numberNights)"
+////        RomType.text = "\(shortNameRoom) \t \(roomPrice)"
+////        wifi.text = "\(wifiDaysPrice)"
+////        total.text = "\(totalPrice)"
+//    }
+    
+    
+    
 }
